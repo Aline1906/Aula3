532f8716c325e4cdf7735ed45690e73419246130
Aline Silva <line_adri@hotmail.com>
2021-05-27 22:14:26 -0300

    Adicionando Atividade 1 da terceira aula

---
+++ atividade1.js
@@ -0,0 +1,11 @@
+//1 - crie um algoritmo que imprime no console de 10 a 60, iterando a cada 5, exceto para os numeros 35 e 45 
+//que irão ser substituidos pela palavra "PULOU".
+
+for (i = 10; i <= 60; i+=5){
+
+    if(i===35 || i===45){
+    console.log("PULOU")
+    continue
+    }
+    console.log(i)
+}
