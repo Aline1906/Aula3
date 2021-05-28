0f01651f47968f83c55d8f46a9af552d0743de93
Aline Silva <line_adri@hotmail.com>
2021-05-27 22:18:46 -0300

    Adicionando Atividade 2 da terceira aula

---
+++ atividade2.js
@@ -0,0 +1,11 @@
+//2 - crie um algoritmo que converte dias em horas, quando recebe um número de dias.
+
+function diasEmHoras (dias){
+
+    let horas = 24
+    let resultado = dias * horas
+
+    return resultado + " horas"
+    
+}
+console.log(diasEmHoras(5))
